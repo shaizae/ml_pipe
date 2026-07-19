90fe3456d171fdcb48ce15fd0219152d3ff25a83
User <shaizae10@gmail.xom>
2026-07-19 22:59:31 +0300

    optimize k folds

--- example.py
+++ example.py
@@ -13,6 +13,7 @@ from utils.utils import FilteringCriteria
 def main():
     train_test_split()
     k_folds()
+    leave_one_out()
 
 
 def train_test_split():
@@ -20,7 +21,6 @@ def train_test_split():
     feature_names = data.feature_names
 
     X = pd.DataFrame(data.data, columns=feature_names)
-    X = X.astype(np.float32)
     target_names = {
         1: "Spruce/Fir",
         2: "Lodgepole Pine",
@@ -37,8 +37,8 @@ def train_test_split():
     test_class.fetchers.min_max_scaler()
     test_class.brut_force_features()
     test_class.train_test_split()
-    filterd_results = test_class.filter_by(FilteringCriteria.accuracy)
-    for i in filterd_results:
+    filtered_results = test_class.filter_by(FilteringCriteria.accuracy)
+    for i in filtered_results:
         i.save_results(r".\tests\pdf")
 
 
@@ -46,9 +46,7 @@ def k_folds():
     data = DB()
     feature_names = data.feature_names
 
-    # Create DataFrame for features
     X = pd.DataFrame(data.data, columns=feature_names)
-    X = X.astype(np.float32)
     target_names = {
         1: "Spruce/Fir",
         2: "Lodgepole Pine",
@@ -65,9 +63,35 @@ def k_folds():
     test_class.fetchers.min_max_scaler()
     test_class.fetcher_selection(chi2, [1, 2, 3])
     test_class.k_folds()
-    filterd_results = test_class.filter_by(FilteringCriteria.accuracy)
-    for i in filterd_results:
+    filtered_results = test_class.filter_by(FilteringCriteria.precision)
+    for i in filtered_results:
         i.save_results(r".\tests\pdf")
+
+
+def leave_one_out():
+    data = DB()
+    feature_names = data.feature_names
+
+    X = pd.DataFrame(data.data, columns=feature_names)
+    target_names = {
+        1: "Spruce/Fir",
+        2: "Lodgepole Pine",
+        3: "Ponderosa Pine",
+        4: "Cottonwood/Willow",
+        5: "Aspen",
+        6: "Douglas-fir",
+        7: "Krummholz"
+    }
+    y = pd.Series(data.target).map(target_names)
+    test_class = Classification()
+
+    test_class.set(X, y, [RandomForestClassifier(), AdaBoostClassifier(), DecisionTreeClassifier(), SVC()])
+    test_class.fetchers.min_max_scaler()
+    test_class.leave_one_out()
+    filtered_results = test_class.filter_by(FilteringCriteria.f1)
+    for i in filtered_results:
+        i.save_results(r".\tests\pdf")
+
 
 
 if __name__ == '__main__':
